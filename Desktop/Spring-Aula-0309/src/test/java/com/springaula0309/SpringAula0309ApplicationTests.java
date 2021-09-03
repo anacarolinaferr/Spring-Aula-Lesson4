8c5d1d5bb5b0278c92f9169d79f8ebb7538baaf0
anacarolinaferr <anacarolina.ferreira@mercadolivre.com>
2021-09-03 11:16:01 -0300

    Cadastro aluno e Busca Aluno

---
+++ Desktop/Spring-Aula-0309/src/test/java/com/springaula0309/SpringAula0309ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springaula0309;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAula0309ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
